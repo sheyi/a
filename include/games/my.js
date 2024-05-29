c2f26530de536faff5f9608fa36cbb9b42e931ec
sheyi_huawei <1912070@qq.com>
2024-05-29 14:28:50 +0800

    add game js

---
+++ include/games/my.js
@@ -0,0 +1,38 @@
+EJS_color = '#00FF80';
+EJS_player = "#playgame";
+
+EJS_color = "#ff6100";
+EJS_pathtodata = "https://cdn.emulatorjs.org/stable/data/";
+
+//EJS_startOnLoaded = true;
+EJS_language = 'zh-CN'; //language
+//EJS_biosUrl = 'neogeo.7z';
+
+//EJS_backgroundImage = 'loadScreen.png';
+EJS_backgroundBlur = true;
+//nes gb gba snes n64 gba nds vb segaMD segaCD sega32x segaMS segaGG segaSaturn 3do atari2600 atari7800 lynx jaguar arcade mame
+////////////////////////////////////////
+EJS_defaultControls = {
+    0: {
+        0: {'value' : 'j', 'value2' : 'BUTTON_2', 'keycode': '74'}, // B
+        // 1: {'value' : '', 'value2' : '', 'keycode': ''}, // Y
+        2: {'value' : 'delete', 'value2' : 'SELECT', 'keycode': '46'}, // SELECT
+        3: {'value' : 'enter', 'value2' : 'START', 'keycode': '13'}, // START
+        4: {'value' : 'w', 'value2' : 'LEFT_STICK_Y:-1', 'keycode': '87'}, // UP
+        5: {'value' : 's', 'value2' : 'LEFT_STICK_Y:+1', 'keycode': '83'}, // DOWN
+        6: {'value' : 'a', 'value2' : 'LEFT_STICK_X:-1', 'keycode': '65'}, // LEFT
+        7: {'value' : 'd', 'value2' : 'LEFT_STICK_X:+1', 'keycode': '68'}, // RIGHT
+        8: {'value' : 'k', 'value2' : 'BUTTON_3', 'keycode': '75'}, // A
+        // 9: {'value' : '', 'value2' : '', 'keycode': ''}, // X
+        10: {'value' : 'i', 'value2' : 'LEFT_TOP_SHOULDER', 'keycode': '73'}, // L
+        11: {'value' : 'l', 'value2' : 'RIGHT_TOP_SHOULDER', 'keycode': '76'}, // R
+        24: {'value' : 'f2', 'value2' : '', 'keycode': '113'}, // QUICK SAVE
+        25: {'value' : 'f4', 'value2' : '', 'keycode': '115'}, // QUICK LOAD
+        27: {'value' : 'shift', 'value2' : '', 'keycode': '16'} // FAST
+        // 28: {'value' : 'r', 'value2' : '', 'keycode': '82'} // SLOW
+    },
+    1: {},
+    2: {},
+    3: {}
+}
+////////////////////////////////////////
